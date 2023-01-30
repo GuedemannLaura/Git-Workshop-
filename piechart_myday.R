201913dd9044bbb1af3a625031664d7225806ac2
GuedemannLaura <78853376+GuedemannLaura@users.noreply.github.com>
2023-01-30 15:47:53 +0000

    first commit to this repo

---
+++ piechart_myday.R
@@ -0,0 +1,13 @@
+#
+# Some example code for the collaboration --------------------------------------
+#
+
+
+# Create data for the graph.
+x <- c(21, 62, 10, 53)
+labels <- c("drinking coffee", "trying to make a ggplot in R", "answering emails", "ranting about how much work ggplots are")
+
+
+# Plot the chart.
+pie(x,labels, main = "My work day")
+
