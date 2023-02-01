c2afc824bc1b1de030f0a5a34e74cec08e3cb159
GuedemannLaura <78853376+GuedemannLaura@users.noreply.github.com>
2023-02-01 12:29:22 +0000

    I uploaded the first version of this R code

---
+++ example_Rcode.R
@@ -0,0 +1,59 @@
+#
+# Git Workshop 1: Example code -------------------------------------------------
+#
+
+#  This is a change in the document 
+
+#
+# load data --------------------------------------------------------------------
+#
+
+data_path <- "C:/Users/lg531/OneDrive - University of Exeter/Studies/Git_workshop_data/"   # please set your directory to data!
+
+load(paste0(data_path, "data.Rdata"))                                            
+
+
+#
+# Check data -------------------------------------------------------------------
+#
+
+str(data)
+
+View(data)
+summary(data)
+
+
+#
+# Linear regression ------------------------------------------------------------
+#
+
+
+fullmodel <- lm(Y ~ X1 + X2 + X3 + X4, data = data)
+summary(fullmodel)
+
+
+#
+# Model selection --------------------------------------------------------------
+# 
+
+
+
+model_selection <- step(fullmodel)
+final_model     <- lm(Y ~ X1 + X2 + X3, data = data)
+
+
+#
+# Assess residuals/ assumptions of final model ---------------------------------
+#
+
+
+par(mfrow = c(2, 2))
+plot(final_model)
+
+
+
+
+
+
+
+
