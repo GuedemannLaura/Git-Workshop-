6fd2b9ea2d0c59e3af77173ec31119e311e725b8
[johnmdennis] <[j.dennis@exeter.ac.uk]>
2023-03-01 13:41:09 +0000

    John correct change

---
+++ piechart_myday_JohnD.R
@@ -0,0 +1,13 @@
+#
+# Some example code for the collaboration --------------------------------------
+#
+#John's changes
+
+# Create data for the graph.
+x <- c(21, 62, 11, 53)
+labels <- c("drinking coffee", "trying to make a ggplot in R", "answering emails", "ranting about how much work ggplots are")
+
+
+# Plot the chart.
+pie(x,labels, main = "My work day")
+
